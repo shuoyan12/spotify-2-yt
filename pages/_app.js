6292ff68569ac51df94472e5ed0b8f9b3d7f593d
shuoyen <xzapwarx@gmail.com>
2021-12-19 10:53:07 +0800

    Login authenticate control.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    //Q: 為啥sessionProvider 要包住component?
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
